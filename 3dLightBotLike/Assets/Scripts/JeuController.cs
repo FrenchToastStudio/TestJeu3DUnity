91e42de6329d53e0ed4f8b075f454430ba5da3ee
Sebastien V <sebvermandele@gmail.com>
2020-04-30 00:12:43 -0400

    fin correction script deplacement sequence - ajout fonction restart niveau

--- 3dLightBotLike/Assets/Scripts/JeuController.cs
+++ 3dLightBotLike/Assets/Scripts/JeuController.cs
@@ -28,30 +28,7 @@ public class JeuController : MonoBehaviour
     private Rigidbody rigidbody;
     private BoxCollider boxCollider;
 
-    bool enMouvement = false;
-    bool go = false;
-    bool marche = false;
-    bool saute = false;
-
-    private List<String> sequence = new List<string>();
-
-    private List<String> historiqueSequence;
-
-    public void setMouvementGauche(){
-        sequence.Add("Gauche");
-    }
-
-    public void setMouvementAvance(){
-        sequence.Add("Avance");         
-    }
-
-    public void setMouvementDroite(){
-        sequence.Add("Droite");  
-    }
-
-    public void setMouvementSaut(){
-        sequence.Add("Saut");  
-    }
+    
 
     void Start()
     {
@@ -62,109 +39,14 @@ public class JeuController : MonoBehaviour
 
     void Update(){
 
-        // if(rigidbody.velocity.y < 0){
-        //     animateur.SetBool("saute", false);
-        // }
-            
-
-        if(personnage.transform.position.z == destination.z || personnage.transform.position.x == destination.x){
-            animateur.SetBool("marche", false);
-            marche = false;
-            enMouvement = false;
-        }
-
-        if(!enMouvement && sequence.Count > 0 && go){
-            Debug.Log("inside if - commande : " + sequence[0]);
-            positionDepart = personnage.transform.position;
-            enMouvement = true;
-            switch(sequence[0]){
-                case "Avance":
-                    destination = personnage.transform.position + (transform.forward);
-                    marche = true;
-                    break;
-                case "Saut":
-                    saut();
-                    saute = true;
-                    break;
-                case "Gauche":
-                    tourne(rotationGauche);
-                    getPositionPersonnage();
-                    enMouvement = false;
-                    break;
-                case "Droite":
-                    tourne(rotationDroite);
-                    getPositionPersonnage();
-                    enMouvement = false;
-                    break;
-
-
-
-            }
-            sequence.RemoveAt(0);
-            
-        }
-
-
-
-        if (marche){
-            animateur.SetBool("marche", true);
-            personnage.transform.position = Vector3.MoveTowards(personnage.transform.position, destination, 1f * Time.deltaTime);
-        }
-
-
-
-
-
-    }
-
-    public void getPositionPersonnage(){
-        positionDepart = personnage.transform.position;
-    }
-
-    public void LancerSequence(){
-        if(sequence.Count == 0 && historiqueSequence.Count > 0)
-            sequence = copyList(historiqueSequence);
-        historiqueSequence = copyList(sequence);
-        go =true;
-    }
-
-    public void saut(){
-        animateur.SetBool("saute", true);
-        animateur.SetBool("estAuSol", false);
-        personnage.GetComponent<Rigidbody>().AddForce(Vector3.up * hauteurSaut, ForceMode.Impulse);
-        personnage.GetComponent<Rigidbody>().AddForce(personnage.transform.forward * uniteDeplacement, ForceMode.Impulse);
-    }
-
-    public void tourne (float rotation){
-        personnage.transform.Rotate(Vector3.up, rotation);
-    }
-
-    void OnCollisionEnter(Collision collision){
-        Debug.Log(saute);
-        if(saute){
-            animateur.SetBool("estAuSol", true);
-            enMouvement = false;
-            saute = false;
-            Debug.Log("est au sol - mouvement false");
-        }
         
-        
-        if(collision.gameObject.tag == "sol"){
-            Time.timeScale = 0;
-            //DisplayMessage(textPerdu);
-        } else if (collision.gameObject.tag == "fin"){
-            Time.timeScale = 0;
-            //DisplayMessage(textGagne);
-        }     
+
+
+
+
     }
 
-    private List<String> copyList (List<String> listeOriginale){
-        List<String> listeDestination = new List<string>();
-        foreach(String valeur in listeOriginale){
-            listeDestination.Add(valeur);
-        }
-        return listeDestination;
-    }
+    
 
 
 
--- 3dLightBotLike/Assets/Scripts/PersonnageController.cs
+++ 3dLightBotLike/Assets/Scripts/PersonnageController.cs
@@ -1,4 +1,4 @@
-﻿using System.Xml.Serialization;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +9,7 @@ public class PersonnageController : MonoBehaviour
     [SerializeField]
     private float uniteDeplacement = 1f;
     [SerializeField]
-    private float hauteurSaut = 4f;
+    private float hauteurSaut = 4.5f;
     [SerializeField]
     private float uniteDeplacementSaut = 1.30f;
     [SerializeField]
@@ -35,7 +35,18 @@ public class PersonnageController : MonoBehaviour
     private Rigidbody rigidbody;
     private BoxCollider boxCollider;
 
-    private bool marche = false;
+    bool enMouvement = false;
+    bool go = false;
+    bool marche = false;
+    bool saute = false;
+
+    private List<String> sequence = new List<string>();
+
+    private List<String> historiqueSequence;
+
+    private GameObject[] listBtnSequence;
+
+    
  
     // Start is called before the first frame update
     void Start()
@@ -45,68 +56,155 @@ public class PersonnageController : MonoBehaviour
 
         // Pour initialiser un restart
         positionDebutNiveau = transform.position;
+        destination = transform.position + (transform.forward);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // rotation = 0;
-        // positionDepart = transform.position;
+        positionDepart = transform.position;
 
-        // if(rigidbody.velocity.y < 0)
-        //     animateur.SetBool("saute", false);
-
-        // if(positionDepart.z == destination.z || positionDepart.x == destination.x){
-        //     animateur.SetBool("marche", false);
-        //     marche = false;
-        // }
+        if(rigidbody.velocity.y < 0){
+            animateur.SetBool("saute", false);
+        }
             
-        // Gestion du déplacement vers l'avant
-        // if(Input.GetKeyDown(KeyCode.W)){
 
-        //     destination = transform.position + (transform.forward);
-        //     marche = true;
-            
-        // }else if(Input.GetKeyDown(KeyCode.X)){
-            
-        //     animateur.SetBool("saute", true);
-        //     animateur.SetBool("estAuSol", false);
-        //     rigidbody.AddForce(Vector3.up * hauteurSaut, ForceMode.Impulse);
-        //     rigidbody.AddForce(transform.forward * uniteDeplacementSaut, ForceMode.Impulse);            
-            
-        // }else if(Input.GetKeyDown(KeyCode.A))
-        //     rotation = rotationGauche;
-        // else if(Input.GetKeyDown(KeyCode.D))
-        //     rotation = rotationDroite;
+        if(transform.position.z == destination.z && transform.position.x == destination.x){
+            Debug.Log("arrive");
+            Debug.Log(transform.position.z + " " + destination.z);
+            animateur.SetBool("marche", false);
+            marche = false;
+            enMouvement = false;
+        }
 
-        // // Actualisation des mouvements
-        // transform.Rotate(Vector3.up, rotation);
+        if(!enMouvement && sequence.Count > 0 && go){
+            Debug.Log("inside if - commande : " + sequence[0]);
+            positionDepart = transform.position;
+            enMouvement = true;
+            switch(sequence[0]){
+                case "Avance":
+                    destination = transform.position + (transform.forward);
+                    marche = true;
+                    break;
+                case "Saut":
+                    saut();
+                    saute = true;
+                    break;
+                case "Gauche":
+                    tourne(rotationGauche);
+                    getPositionPersonnage();
+                    enMouvement = false;
+                    break;
+                case "Droite":
+                    tourne(rotationDroite);
+                    getPositionPersonnage();
+                    enMouvement = false;
+                    break;
+            }
+            sequence.RemoveAt(0);
+        }
 
-        // if (marche){
-        //     animateur.SetBool("marche", true);
-        //     transform.position = Vector3.MoveTowards(transform.position, destination, 0.5f * Time.deltaTime);
-        // } else {
-        //     animateur.SetBool("marche", false);
-        // }        
+           
+        //Gestion du déplacement vers l'avant
+        if(Input.GetKeyDown(KeyCode.W)){
+            destination = transform.position + (transform.forward);
+            marche = true;
+        }else if(Input.GetKeyDown(KeyCode.X)){
+            
+            animateur.SetBool("saute", true);
+            animateur.SetBool("estAuSol", false);
+            rigidbody.AddForce(Vector3.up * hauteurSaut, ForceMode.Impulse);
+            rigidbody.AddForce(transform.forward * uniteDeplacementSaut, ForceMode.Impulse);            
+            
+        }else if(Input.GetKeyDown(KeyCode.A))
+            transform.Rotate(Vector3.up, rotationGauche);
+        else if(Input.GetKeyDown(KeyCode.D))
+            transform.Rotate(Vector3.up, rotationDroite);
+        
+        if (marche){
+            animateur.SetBool("marche", true);
+            transform.position = Vector3.MoveTowards(transform.position, destination, 1f * Time.deltaTime);
+        }       
 
     }
 
-    // void OnCollisionEnter(Collision collision){
-    //     animateur.SetBool("estAuSol", true);
-    //     if(collision.gameObject.tag == "sol"){
-    //         Time.timeScale = 0;
-    //         DisplayMessage(textPerdu);
-    //     } else if (collision.gameObject.tag == "fin"){
-    //         Time.timeScale = 0;
-    //         DisplayMessage(textGagne);
-    //     }     
-    // }
+    void OnCollisionEnter(Collision collision){
+        Debug.Log(saute);
+        animateur.SetBool("estAuSol", true);
+        if(saute){
+            enMouvement = false;
+            saute = false;
+            Debug.Log("est au sol - mouvement false");
+        }
+        if(collision.gameObject.tag == "sol"){
+            Time.timeScale = 0;
+            DisplayMessage(textPerdu);
+        } else if (collision.gameObject.tag == "fin"){
+            Time.timeScale = 0;
+            DisplayMessage(textGagne);
+        }     
+    }
 
     void DisplayMessage(GameObject unGameobject){
         GameObject message = Instantiate(unGameobject) as GameObject;
         message.transform.SetParent(UIgameplay.transform, false);
         message.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0,0,0);
         message.GetComponent<RectTransform>().localScale = new Vector3(4,5,1);
+    }
+
+    public void getPositionPersonnage(){
+        positionDepart = transform.position;
+    }
+
+    public void LancerSequence(){
+        if(sequence.Count == 0 && historiqueSequence.Count > 0)
+            sequence = copyList(historiqueSequence);
+        historiqueSequence = copyList(sequence);
+        go =true;
+    }
+
+    public void recommencerNiveau(){
+        transform.position = positionDebutNiveau;
+        sequence = historiqueSequence = new List<string>();
+        listBtnSequence = GameObject.FindGameObjectsWithTag("btnSequence");
+        foreach(GameObject unBtn in listBtnSequence){
+            Destroy(unBtn);
+        }
+    }
+
+    public void setMouvementGauche(){
+        sequence.Add("Gauche");
+    }
+
+    public void setMouvementAvance(){
+        sequence.Add("Avance");         
+    }
+
+    public void setMouvementDroite(){
+        sequence.Add("Droite");  
+    }
+
+    public void setMouvementSaut(){
+        sequence.Add("Saut");  
+    }
+
+    public void saut(){
+        animateur.SetBool("saute", true);
+        animateur.SetBool("estAuSol", false);
+        GetComponent<Rigidbody>().AddForce(Vector3.up * hauteurSaut, ForceMode.Impulse);
+        GetComponent<Rigidbody>().AddForce(transform.forward * uniteDeplacement, ForceMode.Impulse);
+    }
+
+    public void tourne (float rotation){
+        transform.Rotate(Vector3.up, rotation);
+    }
+
+    private List<String> copyList (List<String> listeOriginale){
+        List<String> listeDestination = new List<string>();
+        foreach(String valeur in listeOriginale){
+            listeDestination.Add(valeur);
+        }
+        return listeDestination;
     }
 }
