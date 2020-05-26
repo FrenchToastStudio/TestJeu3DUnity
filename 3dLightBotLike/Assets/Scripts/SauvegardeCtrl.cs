c72e3b4df5a2b88384d44621de21c2765efa1955
Guillaume <guillaume@chief>
2020-05-26 03:56:09 -0400

    rajout d'un script qui rend les bouton interactable si le nvieau est débloqué

--- 3dLightBotLike/Assets/Scripts/SauvegardeCtrl.cs
+++ 3dLightBotLike/Assets/Scripts/SauvegardeCtrl.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SauvegardeCtrl : MonoBehaviour
 {
     Sauvegarde sauvegarde;
+    [SerializeField]
+    List<GameObject> listeBoutonNiveau;
     // Start is called before the first frame update
     private void Start()
     {
         sauvegarde = GestionaireSauvegardes.Charger();
+        sauvegarde.débloqué[0] = 1;
+        GestionaireSauvegardes.Sauvegarder(sauvegarde);
+
+        //rend les bouton interactable si il sont débloqué
+        if(listeBoutonNiveau.Count > 0) {
+            for(int i = 0; sauvegarde.débloqué.Count > i; i++) {
+                listeBoutonNiveau[i].GetComponent<Button>().interactable = true;
+            }
+        }
     }
 
 
     public void débloquéNiveau() {
-        sauvegarde.débloqué[sauvegarde.position] = 1;
+        Debug.Log(sauvegarde.position);
+        Debug.Log("positon: " + sauvegarde.position);
+        Debug.Log("niveau débloqué!: "+ sauvegarde.débloqué[sauvegarde.position]);
+        sauvegarde.débloqué[sauvegarde.position + 1] = 0;
         GestionaireSauvegardes.Sauvegarder(sauvegarde);
     }
 
