c607c87c29313f4801bf1f9cb9efaf3c35d02ba1
Sebastien V <sebvermandele@gmail.com>
2020-05-27 11:24:26 -0400

    suppression fichiers inutiles et ajout menu résumé fin de niveau

--- 3dLightBotLike/Assets/Scripts/FinNiveauCtrl.cs
+++ 3dLightBotLike/Assets/Scripts/FinNiveauCtrl.cs
@@ -11,13 +11,25 @@ public class FinNiveauCtrl : MonoBehaviour
 {
     [SerializeField] SauvegardeCtrl sauvegardeCtrl;
     [SerializeField] SceneCtrl SceneCtrl;
+    [SerializeField] private GameObject uiGamePlay;
+    [SerializeField] private GameObject uiNiveauReussi;
+    [SerializeField] private GameObject textResumeValeur;
+    [SerializeField] private GameObject textRecordBattu;
 
     private static int nbrDeCoupUtilisé;
     
     void OnCollisionEnter(Collision collision){
         Time.timeScale = 0;
+
+        if(sauvegardeCtrl.getMeilleurScore() > nbrDeCoupUtilisé)
+            textRecordBattu.SetActive(true);
+
         sauvegardeCtrl.GetComponent<SauvegardeCtrl>().débloquéNiveau(nbrDeCoupUtilisé);
-        SceneCtrl.GetComponent<SceneCtrl>().chargerScene("AccueilInterNiveau");
+
+        uiGamePlay.SetActive(false);
+        uiNiveauReussi.SetActive(true);
+        textResumeValeur.GetComponent<Text>().text = nbrDeCoupUtilisé.ToString() + " coups";
+          
     }
 
     public static void setNbrCoupUtilisé(int nbrCoup){
